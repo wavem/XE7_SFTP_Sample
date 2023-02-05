3da3c9a3376dd9d34d3bd2f90797653e76f01214
WAVE <fenix24@naver.com>
2023-02-05 11:26:37 +0900

    [New] Create an new class : TClassDemo

--- SFTP_Sample/Define.h
+++ SFTP_Sample/Define.h
@@ -5,6 +5,27 @@
 //---------------------------------------------------------------------------
 #include <System.Classes.hpp>
 
+#include <stdio.h>
+#include <iostream>
+
+#include "tgputtylibcbclass.h"
+
+class TClassDemo
+{
+  TTGPuttySFTP *PSFTP;
+  public:
+	bool DoVerifyHostKey(const char* host, const int port,
+                         const char * fingerprint, const int verificationstatus,
+                         bool &storehostkey);
+	void DoMessage(const char* Msg, const bool isstderr);
+	bool DoProgress(const __int64 bytescopied, const bool isupload);
+	bool DoListing(const struct fxp_names* names);
+	bool DoGetInput(char* linebuf, const int maxchars);
+	void InitSFTP();
+    void Run();
+};
+
+
 
 
 //---------------------------------------------------------------------------
